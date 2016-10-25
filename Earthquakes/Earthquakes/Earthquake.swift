ee2f752115fe3551410c50518eb9746c87d9a2d8
Pratikbhai Patel <pratikpatel@me.com>
2016-10-25 19:19:31 -0400

    Create earthquake model class

---
+++ Earthquakes/Earthquakes/Earthquake.swift
@@ -0,0 +1,22 @@
+//
+//  Earthquake.swift
+//  Earthquakes
+//
+//  Created by Pratikbhai Patel on 10/25/16.
+//  Copyright © 2016 Pratik Patel. All rights reserved.
+//
+
+import Foundation
+
+class Earthquake {
+    
+    let magnitude: Double = 0
+    let time: Date = Date()
+    let url: URL? = nil
+    let place: String = ""
+    let detail: URL? = nil
+    let longitude: Double = 0
+    let latitude: Double = 0
+    let depth: Double = 0
+
+}
