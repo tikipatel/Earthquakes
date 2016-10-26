88e1fdd86edd8e21986cb1b7e9461f65245d4e9c
delkant <delkant@mburukuja.com>
2016-10-26 19:18:48 -0400

    TableView class added

---
+++ Earthquakes/Earthquakes/ViewController+TableView.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController+TableView.swift
+//  Earthquakes
+//
+//  Created by delkant on 10/26/16.
+//  Copyright © 2016 Pratik Patel. All rights reserved.
+//
+
+import UIKit
+
+extension ViewController{
+
+
+}
