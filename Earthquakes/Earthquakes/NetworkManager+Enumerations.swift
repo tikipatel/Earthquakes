45b230e4cd7a9a8bf3989dd374e3fee3556e0f2a
Pratikbhai Patel <pratikpatel@me.com>
2016-10-25 21:03:47 -0400

    Create the base for our networking layer

---
+++ Earthquakes/Earthquakes/NetworkManager+Enumerations.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager+Enumerations.swift
+//  Earthquakes
+//
+//  Created by Pratikbhai Patel on 10/25/16.
+//  Copyright © 2016 Pratik Patel. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoint: String {
+    case all_day
+}
