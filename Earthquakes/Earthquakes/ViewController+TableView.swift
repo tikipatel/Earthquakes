8cc38df7bb010f21f7fed995752aa7dd75ec9226
Pratikbhai Patel <pratikpatel@me.com>
2016-11-02 18:55:43 -0400

    Refactor code

--- Earthquakes/Earthquakes/ViewController+TableView.swift
+++ Earthquakes/Earthquakes/ViewController+TableView.swift
@@ -9,23 +9,14 @@
 import UIKit
 
 extension ViewController: UITableViewDataSource { //space added for pratik
-
-    var filteredArray: [Earthquake] {// computed property
-        switch currentFilter {
-        case .none: return earthquakes
-        case .gt2: return earthquakes.filter{$0.magnitude > 2.0}
-        case .gt4: return earthquakes.filter{$0.magnitude > 4.0}
-        case .gt6: return earthquakes.filter{$0.magnitude > 6.0}
-        }
-    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredArray.count
+        return earthquakes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! EarthquakeTableViewCell
-        cell.earthquake = filteredArray[indexPath.row]
+        cell.earthquake = earthquakes[indexPath.row]
         return cell
     }
 
@@ -40,7 +31,7 @@ extension ViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let earthquake = filteredArray[indexPath.row]
+        let earthquake = earthquakes[indexPath.row]
         performSegue(withIdentifier: "showEarthquakeDetail", sender: earthquake)
     }
     
